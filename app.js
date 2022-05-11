5ea54f4c379f0565b92cdc0438a0c83e8e5d3392
Showmen Barua <showmen77barua@gmail.com>
2022-05-11 15:45:24 +0600

    vue included and some methods added

--- app.js
+++ app.js
@@ -1,5 +1,26 @@
 const app = Vue.createApp({
-    template: '<h2>Hi from template</h2>'
+    data(){
+        return {
+            name : 'Showmen',
+            age : 2,
+            ageStatus : 'Unknown'
+        }
+    },
+    methods: {
+        increaseAge(){
+            this.age = this.age + 2;
+            if(this.age >= 0 && this.age <= 10){
+                this.ageStatus = 'Child'
+            }
+            if(this.age > 10 && this.age <= 18){
+                this.ageStatus = 'Teen'
+            }
+            if(this.age > 18){
+                this.ageStatus = 'Adult'
+            }
+            
+        }
+    }
 })
 
 app.mount('#app')
